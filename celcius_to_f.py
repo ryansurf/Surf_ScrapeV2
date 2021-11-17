f2cc6b50d863491fd973bb5b500daa400f32aaf6
ryansurf <ryanfrederich@gmail.com>
2021-11-16 19:45:24 -0800

    Fixed issued with temps not being able to differenciate between F and C

--- celcius_to_f.py
+++ celcius_to_f.py
@@ -2,7 +2,11 @@ import re
 #Converts celcius to fahrenheit
 
 def c_to_f(celcius):
-    subbed = re.sub('\D', '', celcius)
-    f = float(subbed) * 1.8 + 32
-    f = round(f, 2)
+    if 'c' in celcius:
+        subbed = re.sub('\D', '', celcius)
+        f = float(subbed) * 1.8 + 32
+        f = round(f, 2)
+    else:
+        subbed = re.sub('\D', '', celcius)
+        f = subbed
     return f
--- meters_to_feet.py
+++ meters_to_feet.py
@@ -2,8 +2,12 @@ import re
 #Converts meters to feet
 
 def m_to_f(tide):
-    subbed = re.sub('[a-z]', '', tide)
-    feet = float(subbed) * 3.28084
-    feet = round(feet, 2)
+    if 'c' in tide:
+        subbed = re.sub('[a-z]', '', tide)
+        feet = float(subbed) * 3.28084
+        feet = round(feet, 2)
+    else:
+        subbed = re.sub('[a-z]', '', tide)
+        feet = subbed
     return feet
 
